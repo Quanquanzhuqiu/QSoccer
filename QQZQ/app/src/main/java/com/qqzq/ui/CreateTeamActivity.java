a8cebc4bb05fda58a01b70f4e50e7d59789f5620
syouketu <syouketu@gmail.com>
2015-08-31 17:57:29 +0800

    add 'create team' page.

---
+++ QQZQ/app/src/main/java/com/qqzq/ui/CreateTeamActivity.java
@@ -0,0 +1,18 @@
+package com.qqzq.ui;
+
+import android.os.Bundle;
+import android.view.Window;
+
+import com.qqzq.BaseActivity;
+import com.qqzq.R;
+
+/**
+ * Created by jie.xiao on 8/31/2015.
+ */
+public class CreateTeamActivity extends BaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_team);
+    }
+}
